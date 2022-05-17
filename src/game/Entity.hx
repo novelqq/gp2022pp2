b6e8eb1cb7aecfd71f13e506e0d86e9299907951
spinachstealer <author.janzen.james@gmail.com>
2022-05-17 13:44:56 -0700

    removed gravity, life, and a few more unneccesary features

--- src/game/Entity.hx
+++ src/game/Entity.hx
@@ -15,6 +15,10 @@ class Entity {
 	var utmod(get,never) : Float; inline function get_utmod() return Game.ME.utmod;
 	public var hud(get,never) : ui.Hud; inline function get_hud() return Game.ME.hud;
 
+	/**	Attributes **/
+	public var isPushable = true;
+	public var isSolid = true;
+
 	/** Cooldowns **/
 	public var cd : dn.Cooldown;
 
@@ -44,11 +48,6 @@ class Entity {
 	/** Y velocity, in grid fractions **/
 	public var dy = 0.;
 
-	/** Uncontrollable bump X velocity, usually applied by external factors (eg. a bumper in Sonic) **/
-    public var bdx = 0.;
-	/** Uncontrollable bump Y velocity, usually applied by external factors (eg. a bumper in Sonic) **/
-	public var bdy = 0.;
-
 	/** Last known X position of the attach point (in pixels), at the beginning of the latest fixedUpdate **/
 	var lastFixedUpdateX = 0.;
 	/** Last known Y position of the attach point (in pixels), at the beginning of the latest fixedUpdate **/
@@ -56,10 +55,6 @@ class Entity {
 
 	/** If TRUE, the sprite display coordinates will be an interpolation between the last known position and the current one. This is useful if the gameplay happens in the `fixedUpdate()` (so at 30 FPS), but you still want the sprite position to move smoothly at 60 FPS or more. **/
 	var interpolateSprPos = true;
-
-	// Velocities + bump velocities
-	public var dxTotal(get,never) : Float; inline function get_dxTotal() return dx+bdx;
-	public var dyTotal(get,never) : Float; inline function get_dyTotal() return dy+bdy;
 
 	/** Multiplier applied on each frame to normal X velocity **/
 	public var frictX = 0.82;
@@ -69,11 +64,6 @@ class Entity {
 	/** Sets both frictX/Y at the same time **/
 	public var frict(never,set) : Float;
 		inline function set_frict(v) return frictX = frictY = v;
-
-	/** Multiplier applied on each frame to bump X velocity **/
-	public var bumpFrictX = 0.93;
-	/** Multiplier applied on each frame to bump Y velocity **/
-	public var bumpFrictY = 0.93;
 
 	/** Pixel width of entity **/
 	public var wid(default,set) : Float = Const.GRID;
@@ -122,21 +112,6 @@ class Entity {
 
 	/** Entity visibility **/
 	public var entityVisible = true;
-
-	/** Current hit points **/
-	public var life(default,null) : Int;
-	/** Max hit points **/
-	public var maxLife(default,null) : Int;
-	/** Last source of damage if it was an Entity **/
-	public var lastDmgSource(default,null) : Null<Entity>;
-
-	/** Horizontal direction (left=-1 or right=1): from "last source of damage" to "this" **/
-	public var lastHitDirFromSource(get,never) : Int;
-	inline function get_lastHitDirFromSource() return lastDmgSource==null ? -dir : -dirTo(lastDmgSource);
-
-	/** Horizontal direction (left=-1 or right=1): from "this" to "last source of damage" **/
-	public var lastHitDirToSource(get,never) : Int;
-		inline function get_lastHitDirToSource() return lastDmgSource==null ? dir : dirTo(lastDmgSource);
 
 	/** Main entity HSprite instance **/
     public var spr : HSprite;
@@ -212,7 +187,6 @@ class Entity {
 		cd = new dn.Cooldown(Const.FPS);
 		ucd = new dn.Cooldown(Const.FPS);
         setPosCase(x,y);
-		initLife(1);
 		state = Normal;
 
         spr = new HSprite(Assets.tiles);
@@ -242,42 +216,8 @@ class Entity {
 		return pivotY;
 	}
 
-	/** Initialize current and max hit points **/
-	public function initLife(v) {
-		life = maxLife = v;
-	}
-
-	/** Inflict damage **/
-	public function hit(dmg:Int, from:Null<Entity>) {
-		if( !isAlive() || dmg<=0 )
-			return;
-
-		life = M.iclamp(life-dmg, 0, maxLife);
-		lastDmgSource = from;
-		onDamage(dmg, from);
-		if( life<=0 )
-			onDie();
-	}
-
-	/** Kill instantly **/
-	public function kill(by:Null<Entity>) {
-		if( isAlive() )
-			hit(life,by);
-	}
-
-	function onDamage(dmg:Int, from:Entity) {}
-
-	function onDie() {
-		destroy();
-	}
-
 	inline function set_dir(v) {
 		return dir = v>0 ? 1 : v<0 ? -1 : dir;
-	}
-
-	/** Return TRUE if current entity wasn't destroyed or killed **/
-	public inline function isAlive() {
-		return !destroyed && life>0;
 	}
 
 	/** Move entity to grid coordinates **/
@@ -365,17 +305,10 @@ class Entity {
 	/** Called when state is changed to a new value **/
 	function onStateChange(old:State, newState:State) {}
 
-
-	/** Apply a bump/kick force to entity **/
-	public function bump(x:Float,y:Float) {
-		bdx += x;
-		bdy += y;
-	}
-
 	/** Reset velocities to zero **/
 	public function cancelVelocities() {
-		dx = bdx = 0;
-		dy = bdy = 0;
+		dx = 0;
+		dy = 0;
 	}
 
 	public function is<T:Entity>(c:Class<T>) return Std.isOfType(this, c);
@@ -391,7 +324,7 @@ class Entity {
 
 	public inline function dirTo(e:Entity) return e.centerX<centerX ? -1 : 1;
 	public inline function dirToAng() return dir==1 ? 0. : M.PI;
-	public inline function getMoveAng() return Math.atan2(dyTotal,dxTotal);
+	public inline function getMoveAng() return Math.atan2(dy,dx);
 
 	/** Return a distance (in grid cells) from this to something **/
 	public inline function distCase(?e:Entity, ?tcx:Int, ?tcy:Int, ?txr=0.5, ?tyr=0.5) {
@@ -520,73 +453,9 @@ class Entity {
 		debugBounds.drawCircle(centerX-attachX, centerY-attachY, 3);
 	}
 
-	/** Wait for `sec` seconds, then runs provided callback. **/
-	function chargeAction(id:String, sec:Float, cb:Void->Void) {
-		if( !isAlive() )
-			return;
-
-		if( isChargingAction(id) )
-			cancelAction(id);
-		if( sec<=0 )
-			cb();
-		else
-			actions.push({ id:id, cb:cb, t:sec});
-	}
-
-	/** If id is null, return TRUE if any action is charging. If id is provided, return TRUE if this specific action is charging nokw. **/
-	public function isChargingAction(?id:String) {
-		if( !isAlive() )
-			return false;
-
-		if( id==null )
-			return actions.length>0;
-
-		for(a in actions)
-			if( a.id==id )
-				return true;
-
-		return false;
-	}
-
-	public function cancelAction(?id:String) {
-		if( !isAlive() )
-			return;
-
-		if( id==null )
-			actions = [];
-		else {
-			var i = 0;
-			while( i<actions.length ) {
-				if( actions[i].id==id )
-					actions.splice(i,1);
-				else
-					i++;
-			}
-		}
-	}
-
-	/** Action management loop **/
-	function updateActions() {
-		if( !isAlive() )
-			return;
-
-		var i = 0;
-		while( i<actions.length ) {
-			var a = actions[i];
-			a.t -= tmod/Const.FPS;
-			if( a.t<=0 ) {
-				actions.splice(i,1);
-				if( isAlive() )
-					a.cb();
-			}
-			else
-				i++;
-		}
-	}
-
 
 	public inline function hasAffect(k:Affect) {
-		return isAlive() && affects.exists(k) && affects.get(k)>0;
+		return affects.exists(k) && affects.get(k)>0;
 	}
 
 	public inline function getAffectDurationS(k:Affect) {
@@ -595,7 +464,7 @@ class Entity {
 
 	/** Add an Affect. If `allowLower` is TRUE, it is possible to override an existing Affect with a shorter duration. **/
 	public function setAffectS(k:Affect, t:Float, ?allowLower=false) {
-		if( !isAlive() || affects.exists(k) && affects.get(k)>t && !allowLower )
+		if(affects.exists(k) && affects.get(k)>t && !allowLower )
 			return;
 
 		if( t<=0 )
@@ -623,9 +492,6 @@ class Entity {
 
 	/** Affects update loop **/
 	function updateAffects() {
-		if( !isAlive() )
-			return;
-
 		for(k in affects.keys()) {
 			var t = affects.get(k);
 			t-=1/Const.FPS * tmod;
@@ -638,11 +504,6 @@ class Entity {
 
 	function onAffectStart(k:Affect) {}
 	function onAffectEnd(k:Affect) {}
-
-	/** Return TRUE if the entity is active and has no status affect that prevents actions. **/
-	public function isConscious() {
-		return !hasAffect(Stun) && isAlive();
-	}
 
 	/** Blink `spr` briefly (eg. when damaged by something) **/
 	public function blink(c:UInt) {
@@ -676,7 +537,6 @@ class Entity {
 		ucd.update(utmod);
 		cd.update(tmod);
 		updateAffects();
-		updateActions();
 
 
 		#if debug
@@ -781,14 +641,14 @@ class Entity {
 		/*
 			Stepping: any movement greater than 33% of grid size (ie. 0.33) will increase the number of `steps` here. These steps will break down the full movement into smaller iterations to avoid jumping over grid collisions.
 		*/
-		var steps = M.ceil( ( M.fabs(dxTotal) + M.fabs(dyTotal) ) / 0.33 );
+		var steps = M.ceil( ( M.fabs(dx) + M.fabs(dy) ) / 0.33 );
 		if( steps>0 ) {
 			var n = 0;
 			while ( n<steps ) {
 				// X movement
-				xr += dxTotal / steps;
+				xr += dx / steps;
 
-				if( dxTotal!=0 )
+				if( dx!=0 )
 					onPreStepX(); // <---- Add X collisions checks and physics in here
 
 				while( xr>1 ) { xr--; cx++; }
@@ -796,9 +656,9 @@ class Entity {
 
 
 				// Y movement
-				yr += dyTotal / steps;
+				yr += dy / steps;
 
-				if( dyTotal!=0 )
+				if( dy!=0 )
 					onPreStepY(); // <---- Add Y collisions checks and physics in here
 
 				while( yr>1 ) { yr--; cy++; }
@@ -810,15 +670,11 @@ class Entity {
 
 		// X frictions
 		dx *= frictX;
-		bdx *= bumpFrictX;
 		if( M.fabs(dx) <= 0.0005 ) dx = 0;
-		if( M.fabs(bdx) <= 0.0005 ) bdx = 0;
 
 		// Y frictions
 		dy *= frictY;
-		bdy *= bumpFrictY;
 		if( M.fabs(dy) <= 0.0005 ) dy = 0;
-		if( M.fabs(bdy) <= 0.0005 ) bdy = 0;
 	}
 
 
--- src/game/sample/SamplePlayer.hx
+++ src/game/sample/SamplePlayer.hx
@@ -10,6 +10,8 @@ package sample;
 
 class SamplePlayer extends Entity {
 	var ca : ControllerAccess<GameAction>;
+	var speed = 0.045;
+	var climbSpeed = 0.;
 	var walkSpeed = 0.;
 
 	// This is TRUE if the player is not falling
@@ -27,7 +29,7 @@ class SamplePlayer extends Entity {
 
 		// Misc inits
 		frictX = 0.84;
-		frictY = 0.94;
+		frictY = 0.84;
 
 		// Camera tracks this
 		camera.trackEntity(this, true);
@@ -69,12 +71,8 @@ class SamplePlayer extends Entity {
 		super.onPreStepY();
 
 		// Land on ground
-		if( yr>1 && level.hasCollision(cx,cy+1) ) {
-			setSquashY(0.5);
-			dy = 0;
-			yr = 1;
-			ca.rumble(0.2, 0.06);
-			onPosManuallyChangedY();
+		if( yr>0.8 && level.hasCollision(cx,cy+1) ) {
+			yr = 0.8;
 		}
 
 		// Ceiling collision
@@ -91,23 +89,17 @@ class SamplePlayer extends Entity {
 		super.preUpdate();
 
 		walkSpeed = 0;
-		if( onGround )
-			cd.setS("recentlyOnGround",0.1); // allows "just-in-time" jumps
-
-
-		// Jump
-		if( cd.has("recentlyOnGround") && ca.isPressed(Jump) ) {
-			dy = -0.85;
-			setSquashX(0.6);
-			cd.unset("recentlyOnGround");
-			fx.dotsExplosionExample(centerX, centerY, 0xffcc00);
-			ca.rumble(0.05, 0.06);
-		}
+		climbSpeed = 0;
 
 		// Walk
 		if( ca.getAnalogDist2(MoveLeft,MoveRight)>0 ) {
 			// As mentioned above, we don't touch physics values (eg. `dx`) here. We just store some "requested walk speed", which will be applied to actual physics in fixedUpdate.
 			walkSpeed = ca.getAnalogValue2(MoveLeft,MoveRight); // -1 to 1
+		}
+
+		// Climb
+		if(ca.getAnalogDist2(MoveUp,MoveDown)>0 ) {
+			climbSpeed = ca.getAnalogValue2(MoveUp,MoveDown);
 		}
 	}
 
@@ -115,14 +107,12 @@ class SamplePlayer extends Entity {
 	override function fixedUpdate() {
 		super.fixedUpdate();
 
-		// Gravity
-		if( !onGround )
-			dy+=0.05;
-
 		// Apply requested walk movement
 		if( walkSpeed!=0 ) {
-			var speed = 0.045;
 			dx += walkSpeed * speed;
+		}
+		if( climbSpeed!=0) {
+			dy += climbSpeed * speed;
 		}
 	}
 }
